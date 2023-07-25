8b16a24c9013f24d578aec4967c676fb3efb6320
vahitkincir <vahitkincir@gmail.com>
2023-07-25 15:31:54 +0300

    initial commit

---
+++ wall/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace wall.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
